d7602b1c29992e34cc04f415fcdd963f1b0beb91
lipcoder <lipcoder@foxmail.com>
2026-02-26 15:09:57 +0800

    archinstall好像没必要全自动

--- 1.archinstall.sh
+++ 1.archinstall.sh
@@ -24,14 +24,25 @@ ip link
 timedatectl set-ntp true
 timedatectl set-local-rtc true  # 让 Linux 认为硬件时间是当地时间
 
-#当前我们选择清华源
+# 当前我们选择清华源
 sed -i '1iServer = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 #也可以直接到文件里面设置
 
 # 将基准包安装到/mnt目标目录中，然后生成新的系统
 pacstrap /mnt base base-devel \
-        cmake arch-install-scripts vim curl wget \
-        rsync net-tools iwd inetutils bind btrfs-progs
+        cmake arch-install-scripts vim curl  \
+        iproute2 inetutils bind btrfs-progs
+
+# base                  - 系统最小基础环境（能启动、能进 shell、基础工具）
+# base-devel            - 编译/构建工具链包组（make/gcc 等，便于编译软件与用 AUR）
+# cmake                 - 构建系统生成器（很多源码/软件编译会用到）
+# arch-install-scripts  - Arch 安装辅助工具（pacstrap/genfstab/arch-chroot）    -生成fstab
+# vim                   - 终端文本编辑器
+# curl                  - 命令行 HTTP(S) 客户端（下载、请求接口、测试服务）
+# iproute2              - 现代网络/链路/路由管理工具（ip、ss 等，替代 net-tools）   -ip a查看ip还有网卡情况
+# inetutils             - 常用网络小工具集合（如 ping、hostname、telnet 等）
+# bind                  - DNS 查询/诊断工具（dig、host、nslookup）
+# btrfs-progs           - Btrfs 文件系统工具（创建、子卷/快照管理、检查维护）
 
 # 挂载home分区
 mkfs.btrfs /dev/nvme0n1p5
@@ -40,8 +51,8 @@ mkdir /mnt/home
 mount -t btrfs /dev/nvme0n1p5 /mnt/home
 # -t btrfs  手指定为btrfs格式，更加安全
 
-#生成fstab条目
-genfstab -U /mnt >> /mnt/etc/fstab      #这个方法可能会导致一个结果是把把另一个分区也变成了/切记要检查一下
+# 生成fstab条目
+genfstab -U /mnt >> /mnt/etc/fstab      # 这个方法可能会导致一个结果是把把另一个分区也变成了/切记要检查一下
 # -U 使用分区的UUID　 
 # /mnt 要扫描的目录
 # >> /mnt/etc/fstab 追加到目标文件fstab的末尾,下面是示例
@@ -52,15 +63,15 @@ genfstab -U /mnt >> /mnt/etc/fstab      #这个方法可能会导致一个结果
 # UUID=dba67df7-de20-48ae-a714-053d7febd8f4	/home         	btrfs     	rw,relatime,ssd,discard=async,space_cache=v2,subvol=/	0 0
 # =============================================================================
 
-#安装引导
+# 安装引导
 sudo pacman -S refind
-umount /mnt && mount /dev/nvme0n1p1 /mnt && cd /mnt
+umount /mnt && mount /dev/nvme0n1p1 /mnt
 refind-install --alldrivers
 # 经过多次的验证，这个办法最为可行
-#usedefault 代表不进入交互模式
-#alldrivers包括了更多的驱动，原来可能只有ext4
+# usedefault 代表不进入交互模式
+# alldrivers包括了更多的驱动，原来可能只有ext4
 
-cat > ./EFI/refind/refind.config <<'EOF'
+cat > /mnt/EFI/refind/refind.config <<'EOF'
 timeout 20
 scanfor external,optical,manual
 
@@ -85,37 +96,37 @@ EOF
 
 # =============================================================================
 
-#切换到目标　root
+# 切换到目标　root
 umount /mnt && mount /dev/nvme0n1p4 /mnt && arch-chroot /mnt /bin/bash
 
 set -xeu  
-#-x（xtrace）：执行每一行命令前，把“将要执行的命令”打印到标准错误，便于调试。
-#-e（errexit）：脚本中任一简单命令返回非零状态时，立即退出（有若干例外，见下面“坑点”）。
-#-u（nounset）：使用未定义变量时当作错误并退出（如 $foo 未设定）。
-# 在这个模式下运行时如果报错会退出虚拟环境，在敲命令时一定要注意是否在虚拟环境里面
+# -x（xtrace）：执行每一行命令前，把“将要执行的命令”打印到标准错误，便于调试。
+# -e（errexit）：脚本中任一简单命令返回非零状态时，立即退出
+# -u（nounset）：使用未定义变量时当作错误并退出（如 $foo 未设定）。
+#  在这个模式下运行时如果报错会退出虚拟环境，在敲命令时一定要注意是否在虚拟环境里面
 
 useradd -m aria
-echo 'aria ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers  #在aria用户登录状态下sudo命令不需要root密码
-passwd root #添加root的密码
-passwd aria  #添加aria用户的密码
+echo 'aria ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers  # 在aria用户登录状态下sudo命令不需要root密码
+passwd root     # 添加root的密码
+passwd aria     # 添加aria用户的密码
 
-echo 'arch' > /etc/hostname #修改主机的名字
+echo 'arch' > /etc/hostname # 修改主机的名字
 
 # 调整当前系统显示时间为正确时间/时区，再将系统显示时间写入BIOS时间
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && hwclock -w
 
-#开启需要的locale
+# 开启需要的locale
 sed -i 's/^#\(en_US.UTF-8 UTF-8\)/\1/' /etc/locale.gen
 sed -i 's/^#\(zh_CN.UTF-8 UTF-8\)/\1/' /etc/locale.gen
-#可以使用 vim /etc/locale.gen,用/命令搜索
-#en_US.UTF-8 UTF-8
-#zh_CN.UTF-8 UTF-8
-#zh_TW.UTF-8 UTF-8
+# 可以使用 vim /etc/locale.gen,用/命令搜索
+# en_US.UTF-8 UTF-8
+# zh_CN.UTF-8 UTF-8
+# zh_TW.UTF-8 UTF-8
 # 然后按x删除#取消注释
 
 locale-gen && echo 'LANG=en_US.UTF-8' > /etc/locale.conf # 更新对应的 locale 文件,设置系统的默认语言环境
 
-#配置镜像源
+# 配置镜像源
 cat > /etc/pacman.d/mirrorlist <<'EOF'
 Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch
 Server = https://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch
@@ -133,18 +144,18 @@ pacman -S archlinuxcn-keyring pacman-contrib
 
 pacman -Sy && pacman -Fy 
 
-#根据cpu来安装对应的微码包
+# 根据cpu来安装对应的微码包
 if grep /proc/cpuinfo -qs -e 'GenuineIntel'; then
     pacman -S intel-ucode
 elif grep /proc/cpuinfo -qs -e 'AuthenticAMD'; then
     pacman -S amd-ucode
 fi
 
-#安装需要的内核
+# 安装需要的内核
 pacman -S linux linux-headers linux-firmware
 
-#安装显示服务器以及显示管理器
-sudo pacman -S xorg     #echo $XDG_SESSION_TYPE 查看当前使用的桌面
+# 安装显示服务器以及显示管理器
+sudo pacman -S xorg     # echo $XDG_SESSION_TYPE 查看当前使用的桌面
 sudo pacman -S sddm
 sudo systemctl enable sddm
 
@@ -154,7 +165,6 @@ sudo pacman -S plasma-desktop
 # 必要的桌面软件
 sudo pacman -S plasma-systemmonitor dolphin gparted yay firefox konsole
 
-#安装网络服务
-sudo pacman -S plasma-nm
-sudo pacman -S networkmanager
+# 安装网络服务
+sudo pacman -S --needed networkmanager plasma-nm wpa_supplicant
 sudo systemctl enable NetworkManager
