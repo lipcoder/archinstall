c31893bd24b97d528ecfd4fe2401f569b6844d7d
lipcoder <lipcoder@foxmail.com>
2026-02-26 23:59:13 +0800

    nothing

--- 4.python.sh
+++ 4.python.sh
@@ -10,13 +10,13 @@ else
 fi
 
 # 手动版本
-# python -m venv .venv							-创建虚拟环境
-# echo 'source .venv/bin/activate' > .envrc  	-在当前目录生成.envrc 文件
-# direnv allow								 	-授权direnv使用.envrc，完成后可自动进出
-# source .venv/bin/activate					 	-激活这个虚拟环境
-# deactivate									-退出虚拟环境
-# pyenv install -s 3.14.3						-下载特定版本
-# pyenv local 3.12.7							-在当前目录写入.python-version
+# python -m venv .venv                     #创建虚拟环境
+# echo 'source .venv/bin/activate' >.envrc #在当前目录生成.envrc
+# direnv allow                             #授权direnv使用.envrc，完成后可自动进出
+# source .venv/bin/activate                #激活这个虚拟环境
+# deactivate                               #退出虚拟环境
+# pyenv install -s 3.14.3                  #下载特定版本
+# pyenv local 3.12.7                       #在当前目录写入.python-version
 
 # 控制python版本
 cat >>~/.bashrc <<'EOF'
