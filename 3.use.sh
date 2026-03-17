531db556e39e0b29fc73bf2a9db910c40856d105
lipmod <lipmod@qq.com>
2026-03-17 14:31:28 +0800

    nothing

--- 3.use.sh
+++ 3.use.sh
@@ -1,7 +1,7 @@
 # 本文件用于安装需要的文件，和配置一些软件
 
 # 基础软件
-sudo pacman -S ark kitty
+sudo pacman -S ark kitty yay firefox gparted
 
 # bash美化
 # __git_ps1 是 Git 自带的一个函数，用来在提示符里显示当前目录的 Git 分支
@@ -15,12 +15,6 @@ fi
 source ~/sh/powerbash.sh
 EOF
 source ~/.bashrc
-
-# 禁用密码管理器
-sed -i '/^\[Wallet\]$/,/^\[/{s/^Enabled=.*/Enabled=false/}' ~/.config/kwalletrc
-# vim ~/.config/kwalletrc
-# [Wallet]
-# Enabled=false
 
 # 配置蓝牙
 sudo pacman -S bluez bluez-utils bluedevil
@@ -51,14 +45,8 @@ sudo pacman -S openbsd-netcat
 sudo pacman -S ffmpeg
 # ffmpeg -i video.mp4 -i audio.mp3 -c:v copy -map 0:v:0 -map 1:a:0 -shortest output.mp4 # 替换音频
 
-# 游戏
-sudo pacman -S hmcl #mc
-
 # 找包
 yay -S pacseek # 使用也是命令行输入pacseek
-
-# 测试软件
-sudo pacman -S vkmark
 
 # 网络下载工具和同步工具
 sudo pacman -S wget rsync
