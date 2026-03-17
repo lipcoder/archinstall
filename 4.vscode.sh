016c24c3a2dd20f03e589887b1a2cc126d7d6b30
lipcoder <lipcoder@foxmail.com>
2026-03-17 18:28:10 +0800

    迁移配置文件

--- 4.vscode.sh
+++ 4.vscode.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# 导出当前插件(code --list-extensions > vscode-extensions.txt)
+# 导入插件
 set -euo pipefail
 
 while IFS= read -r ext; do
@@ -8,5 +10,8 @@ done < vscode-extensions.txt
 
 echo "安装完成"
 
-# code --list-extensions > vscode-extensions.txt
-# 导出当前的插件
+# 导入配置文件
+cp .vscode/settings.json "$HOME/.config/Code/User/settings.json"
+cp .vscode/keybindings.json "$HOME/.config/Code/User/keybindings.json"
+
+echo "已恢复到 $HOME/.config/Code/User/"
