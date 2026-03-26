dad9fbc490807472fd456e0ba88be517eeb3709f
lipcoder <lipcoder@foxmail.com>
2026-03-26 20:49:31 +0800

    nothing

--- 2.chinese.sh
+++ 2.chinese.sh
@@ -2,6 +2,7 @@
 sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 #安装输入法
+sudo pacman -S ibus ibus-libpinyin # 这个与gnome更加般配
 sudo pacman -S fcitx5-im fcitx5-rime
 sudo pacman -S rime-pinyin-simp
 
