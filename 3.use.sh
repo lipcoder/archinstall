88213ccc722f8645b7d2b6420f6c1ead24e07bd6
lipcoder <lipcoder@foxmail.com>
2026-04-25 21:30:39 +0800

    近期的一些修改

--- 3.use.sh
+++ 3.use.sh
@@ -16,7 +16,7 @@ alias gitl="git log -M --graph --color=always --pretty=format:'%Cred%h%Creset -%
 alias gits="git status --ignore-submodules='dirty' -sb -uall"
 alias gita='git add'
 function gitm() {
-  git commit -m "$*"
+	git commit -m "$*"
 }
 EOF
 mkdir $HOME/sh/ && cd $HOME/sh/
@@ -42,3 +42,120 @@ sudo pacman -S bluedevil
 
 # vscode
 yay -S visual-studio-code-bin
+
+# bash美化
+# 要使用的工具和字体
+sudo pacman -S starship ttf-cascadia-code-nerd
+mkdir -p ~/.config/kitty && echo "font_family CaskaydiaCove Nerd Font" >>~/.config/kitty/kitty.conf
+grep -qxF 'eval "$(starship init bash)"' ~/.bashrc || echo 'eval "$(starship init bash)"' >>~/.bashrc
+
+# 备份配置
+[ -f ~/.config/starship.toml ] && cp ~/.config/starship.toml ~/.config/starship.toml.bak.$(date +%s)
+
+cat >~/.config/starship.toml <<'EOF'
+"$schema" = "https://starship.rs/config-schema.json"
+
+add_newline = true
+command_timeout = 1000
+
+format = """
+[](fg:#7aa2f7)\
+$os\
+[](fg:#7aa2f7 bg:#89ddff)\
+$username\
+[](fg:#89ddff bg:#7dcfff)\
+$directory\
+($git_branch$git_status[](fg:#5e81ac bg:#b4befe))\
+${custom.no_git_sep}\
+$time\
+[](fg:#b4befe)\
+${custom.venv_plain}
+$character"""
+
+[os]
+disabled = false
+style = "fg:#1e1e2e bg:#7aa2f7 bold"
+format = "[ $symbol ]($style)"
+
+[os.symbols]
+Arch = ""
+Linux = ""
+Ubuntu = ""
+Debian = ""
+Fedora = ""
+Macos = ""
+Windows = ""
+
+[username]
+show_always = true
+style_user = "fg:#1e1e2e bg:#89ddff bold"
+style_root = "fg:#1e1e2e bg:#89ddff bold"
+format = "[ $user ]($style)"
+
+[directory]
+style = "fg:#1e1e2e bg:#7dcfff bold"
+format = "[ $path ]($style)"
+truncation_length = 2
+truncation_symbol = ".../"
+home_symbol = "~"
+
+[git_branch]
+style = "fg:#eceff4 bg:#5e81ac bold"
+format = "[](fg:#7dcfff bg:#5e81ac)[ $branch]($style)"
+
+[git_status]
+style = "fg:#eceff4 bg:#5e81ac bold"
+format = "([~$all_status$ahead_behind ]($style))"
+
+conflicted = "="
+ahead = "⇡"
+behind = "⇣"
+diverged = "⇕"
+up_to_date = ""
+untracked = "?"
+stashed = "$"
+modified = "!"
+staged = "+"
+renamed = "»"
+deleted = "x"
+
+[custom.no_git_sep]
+when = 'if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then exit 1; else exit 0; fi'
+command = 'printf ""'
+style = "fg:#7dcfff bg:#b4befe"
+format = "[$output]($style)"
+shell = ["bash", "-c"]
+use_stdin = false
+
+[time]
+disabled = false
+time_format = "%H:%M"
+style = "fg:#1e1e2e bg:#b4befe bold"
+format = "[ $time ]($style)"
+
+[custom.venv_plain]
+when = 'test -n "$VIRTUAL_ENV" || test -n "$CONDA_DEFAULT_ENV"'
+command = '''
+if [ -n "$VIRTUAL_ENV" ]; then
+  name="$(basename "$VIRTUAL_ENV")"
+  case "$name" in
+    .venv|venv|env)
+      basename "$(dirname "$VIRTUAL_ENV")"
+      ;;
+    *)
+      printf "%s" "$name"
+      ;;
+  esac
+elif [ -n "$CONDA_DEFAULT_ENV" ]; then
+  printf "%s" "$CONDA_DEFAULT_ENV"
+fi
+'''
+style = "fg:#b4befe bold"
+format = "[ ~ $output]($style)"
+shell = ["bash", "-c"]
+use_stdin = false
+
+[character]
+success_symbol = "[❯](bold #7dcfff)"
+error_symbol = "[❯](bold #f7768e)"
+EOF
